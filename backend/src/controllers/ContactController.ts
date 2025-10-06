7eb58aeb4332d56f40dbd141873d96f30a1dd328
Leandro Alves Correia <motivadoufop2021@gmail.com>
2025-10-06 20:45:42 -0300

    updateupdate

--- backend/src/controllers/ContactController.ts
+++ backend/src/controllers/ContactController.ts
@@ -2,24 +2,17 @@ import * as Yup from "yup";
 import { Request, Response } from "express";
 import { getIO } from "../libs/socket";
 
-import Contact from "../models/Contact";
 import ListContactsService from "../services/ContactServices/ListContactsService";
 import CreateContactService from "../services/ContactServices/CreateContactService";
 import ShowContactService from "../services/ContactServices/ShowContactService";
 import UpdateContactService from "../services/ContactServices/UpdateContactService";
 import DeleteContactService from "../services/ContactServices/DeleteContactService";
-import GetContactService from "../services/ContactServices/GetContactService";
 
-import CheckContactNumber from "../services/WbotServices/CheckNumber";
+import CheckContactNumber from "../services/WbotServices/CheckNumber"
 import CheckIsValidContact from "../services/WbotServices/CheckIsValidContact";
 import GetProfilePicUrl from "../services/WbotServices/GetProfilePicUrl";
 import AppError from "../errors/AppError";
-import SimpleListService, {
-  SearchContactParams
-} from "../services/ContactServices/SimpleListService";
-import ContactCustomField from "../models/ContactCustomField";
-import {head} from "lodash";
-import {ImportContacts} from "../services/ContactServices/ImportContacts";
+import GetContactService from "../services/ContactServices/GetContactService";
 
 type IndexQuery = {
   searchParam: string;
@@ -31,7 +24,7 @@ type IndexGetContactQuery = {
   number: string;
 };
 
-interface ExtraInfo extends ContactCustomField {
+interface ExtraInfo {
   name: string;
   value: string;
 }
@@ -44,35 +37,27 @@ interface ContactData {
 
 export const index = async (req: Request, res: Response): Promise<Response> => {
   const { searchParam, pageNumber } = req.query as IndexQuery;
-  const { companyId } = req.user;
 
   const { contacts, count, hasMore } = await ListContactsService({
     searchParam,
-    pageNumber,
-    companyId
+    pageNumber
   });
 
   return res.json({ contacts, count, hasMore });
 };
 
-export const getContact = async (
-  req: Request,
-  res: Response
-): Promise<Response> => {
+export const getContact = async (req: Request, res: Response): Promise<Response> => {
   const { name, number } = req.body as IndexGetContactQuery;
-  const { companyId } = req.user;
 
   const contact = await GetContactService({
     name,
-    number,
-    companyId
+    number
   });
 
   return res.status(200).json(contact);
 };
 
 export const store = async (req: Request, res: Response): Promise<Response> => {
-  const { companyId } = req.user;
   const newContact: ContactData = req.body;
   newContact.number = newContact.number.replace("-", "").replace(" ", "");
 
@@ -85,41 +70,30 @@ export const store = async (req: Request, res: Response): Promise<Response> => {
 
   try {
     await schema.validate(newContact);
-  } catch (err: any) {
+  } catch (err) {
     throw new AppError(err.message);
   }
 
-  await CheckIsValidContact(newContact.number, companyId);
-  const validNumber = await CheckContactNumber(newContact.number, companyId);
-  const number = validNumber.jid.replace(/\D/g, "");
-  newContact.number = number;
-
-    // Check if the contact already exists
-    const existingContact = await Contact.findOne({
-      where: {
-        number: newContact.number,
-        companyId
-      }
-    });
-    
-    if (existingContact) {
-      // Contact already exists, send the existing contact data as the response
-      return res.status(200).json({ alreadyExists: true, existingContact });
-    }
-
-  /**
-   * Código desabilitado por demora no retorno
-   */
-  // const profilePicUrl = await GetProfilePicUrl(validNumber.jid, companyId);
+  await CheckIsValidContact(newContact.number);
+  const validNumber : any = await CheckContactNumber(newContact.number)
+  
+  //const profilePicUrl = await GetProfilePicUrl(validNumber);
+  const profilePicUrl = "/static/media/foto.jpg";
+  let name = newContact.name
+  let number = validNumber
+  let email = newContact.email
+  let extraInfo = newContact.extraInfo
 
   const contact = await CreateContactService({
-    ...newContact,
-    // profilePicUrl,
-    companyId
+    name,
+    number,
+    email,
+    extraInfo,
+    profilePicUrl
   });
 
   const io = getIO();
-  io.to(`company-${companyId}-mainchannel`).emit(`company-${companyId}-contact`, {
+  io.emit("contact", {
     action: "create",
     contact
   });
@@ -129,9 +103,8 @@ export const store = async (req: Request, res: Response): Promise<Response> => {
 
 export const show = async (req: Request, res: Response): Promise<Response> => {
   const { contactId } = req.params;
-  const { companyId } = req.user;
 
-  const contact = await ShowContactService(contactId, companyId);
+  const contact = await ShowContactService(contactId);
 
   return res.status(200).json(contact);
 };
@@ -141,7 +114,6 @@ export const update = async (
   res: Response
 ): Promise<Response> => {
   const contactData: ContactData = req.body;
-  const { companyId } = req.user;
 
   const schema = Yup.object().shape({
     name: Yup.string(),
@@ -153,25 +125,18 @@ export const update = async (
 
   try {
     await schema.validate(contactData);
-  } catch (err: any) {
+  } catch (err) {
     throw new AppError(err.message);
   }
 
-  await CheckIsValidContact(contactData.number, companyId);
-  const validNumber = await CheckContactNumber(contactData.number, companyId);
-  const number = validNumber.jid.replace(/\D/g, "");
-  contactData.number = number;
+  await CheckIsValidContact(contactData.number);
 
   const { contactId } = req.params;
 
-  const contact = await UpdateContactService({
-    contactData,
-    contactId,
-    companyId
-  });
+  const contact = await UpdateContactService({ contactData, contactId });
 
   const io = getIO();
-  io.to(`company-${companyId}-mainchannel`).emit(`company-${companyId}-contact`, {
+  io.emit("contact", {
     action: "update",
     contact
   });
@@ -184,76 +149,14 @@ export const remove = async (
   res: Response
 ): Promise<Response> => {
   const { contactId } = req.params;
-  const { companyId } = req.user;
-
-  await ShowContactService(contactId, companyId);
 
   await DeleteContactService(contactId);
 
   const io = getIO();
-  io.to(`company-${companyId}-mainchannel`).emit(`company-${companyId}-contact`, {
+  io.emit("contact", {
     action: "delete",
     contactId
   });
 
   return res.status(200).json({ message: "Contact deleted" });
-};
-
-export const list = async (req: Request, res: Response): Promise<Response> => {
-  const { name } = req.query as unknown as SearchContactParams;
-  const { companyId } = req.user;
-
-  const contacts = await SimpleListService({ name, companyId });
-
-  return res.json(contacts);
-};
-
-export const upload = async (req: Request, res: Response) => {
-  const files = req.files as Express.Multer.File[];
-  const file: Express.Multer.File = head(files) as Express.Multer.File;
-  const { companyId } = req.user;
-
-  const response = await ImportContacts(companyId, file);
-
-  const io = getIO();
-
-  io.to(`company-${companyId}-mainchannel`).emit(`company-${companyId}-contact`, {
-    action: "create",
-    records: response
-  });
-
-  return res.status(200).json(response);
-};
-
-export const getContactVcard = async (
-  req: Request,
-  res: Response
-): Promise<Response> => {
-  const { name, number } = req.query as IndexGetContactQuery;
-  const { companyId } = req.user;
-
-  let vNumber = number;
-  const numberDDI = vNumber.toString().substr(0, 2);
-  const numberDDD = vNumber.toString().substr(2, 2);
-  const numberUser = vNumber.toString().substr(-8, 8);
-
-  if (numberDDD <= '30' && numberDDI === '55') {
-    console.log("menor 30")
-    vNumber = `${numberDDI + numberDDD + 9 + numberUser}@s.whatsapp.net`;
-  } else if (numberDDD > '30' && numberDDI === '55') {
-    console.log("maior 30")
-    vNumber = `${numberDDI + numberDDD + numberUser}@s.whatsapp.net`;
-  } else {
-    vNumber = `${number}@s.whatsapp.net`;
-  }
-
-  console.log(vNumber);
-
-  const contact = await GetContactService({
-    name,
-    number,
-    companyId
-  });
-
-  return res.status(200).json(contact);
 };
